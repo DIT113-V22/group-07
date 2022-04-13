ee53c5425ed041a441231e6371294fc0eb6f981e
Adrian Niklasson <gusnikada@student.gu.se>
2022-04-13 21:03:34 +0200

    mqtt code for the arduino side

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,24 +1,40 @@
+#include <vector>
+
+#include <MQTT.h>
+#include <WiFi.h>
+#ifdef __SMCE__
+#include <OV767X.h>
+#endif
+
 #include <Smartcar.h>
 
-ArduinoRuntime arduinoRuntime;
+MQTTClient mqtt;
+WiFiClient net;
 
-//Motors
+
+ArduinoRuntime arduinoRuntime;
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
 
-const int triggerPin           = 6; 
-const int echoPin              = 7; 
-const unsigned int maxDistance = 100;
+const auto oneSecond = 1000UL;
+#ifdef __SMCE__
+const auto triggerPin = 6;
+const auto echoPin = 7;
+const auto mqttBrokerUrl = "127.0.0.1";
 const auto pulsesPerMeter = 600;
-const int lDegree = -25; // only 25* so it doesnt turn so much
-const int rDegree = 25;
-
+#else
+const auto triggerPin = 33;
+const auto echoPin = 32;
+const auto mqttBrokerUrl = "192.168.0.40";
+#endif
+const unsigned int maxDistance = 100;
 
 //Top Sensor
 GY50 gyroscope(arduinoRuntime, 37);
+
 //Ultrasonic
-SR04 front{arduinoRuntime, triggerPin, echoPin, maxDistance};
+SR04 front(arduinoRuntime, triggerPin, echoPin, maxDistance);
 
 //Odometer
 DirectionlessOdometer leftOdometer{ arduinoRuntime,
@@ -30,21 +46,52 @@ DirectionlessOdometer rightOdometer{ arduinoRuntime,
                                      []() { rightOdometer.update(); },
                                      pulsesPerMeter };
 
+std::vector<char> frameBuffer;
+
 SmartCar car(arduinoRuntime, control, gyroscope, leftOdometer, rightOdometer);
 
-void setup()
-{
-    Serial.begin(9600);
-    carGo();
+
+
+void setup() {
+  Serial.begin(9600);
+  carGo();
   
-}
-//car is moving at 60$% of its speed
-void carGo() {
-        car.setSpeed(60);
+#ifdef __SMCE__
+  Camera.begin(QVGA, RGB888, 15);
+  frameBuffer.resize(Camera.width() * Camera.height() * Camera.bytesPerPixel());
+#endif
 
+  WiFi.begin();
+  mqtt.begin(mqttBrokerUrl, 1883, net);
+
+  Serial.println("Connecting to WiFi...");
+  auto wifiStatus = WiFi.status();
+  while (wifiStatus != WL_CONNECTED && wifiStatus != WL_NO_SHIELD) {
+    Serial.println(wifiStatus);
+    Serial.print(".");
+    delay(1000);
+    wifiStatus = WiFi.status();
+  }
+
+
+  Serial.println("Connecting to MQTT broker");
+  while (!mqtt.connect("arduino", "public", "public")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  mqtt.subscribe("/smartcar/control/#", 1);
+  mqtt.onMessage([](String topic, String message) {
+    if (topic == "/smartcar/control/throttle") {
+      car.setSpeed(message.toInt());
+    } else if (topic == "/smartcar/control/steering") {
+      car.setAngle(message.toInt());
+    } else {
+      Serial.println(topic + " " + message);
+    }
+  });
 }
 
-//detects obstacles within distance range
 void obstacle() {
     const auto distance = front.getDistance();
  if (distance > 0 && distance < 100) {
@@ -52,36 +99,35 @@ void obstacle() {
  }
 }
 
-// input A D makes the car turn left / right X degrees
-void input(){
-    if (Serial.available())
-    {
-        char input = Serial.read(); // read everything that has been received so far and log down
-                                    // the last entry
-        switch (input)
-        {
-        case 'a': // rotate counter-clockwise going forward
-            car.setSpeed(60);
-            car.setAngle(lDegree);
-            break;
-        case 'd': // turn clock-wise
-            car.setSpeed(60);
-            car.setAngle(rDegree);
-            break;
-        default: // if you receive something that you don't know, just stop
-            car.setSpeed(0);
-            car.setAngle(0);
-        }
-    }
+void carGo() {
+      car.setSpeed(60);
 }
 
- void loop()
-{
-   obstacle();
-   input();
-    
+void loop() {
+ 
+  obstacle();
+  
+  if (mqtt.connected()) {
+    mqtt.loop();
+    const auto currentTime = millis();
 #ifdef __SMCE__
-//avoid overloading of cpu when emulator is running
+    static auto previousFrame = 0UL;
+    if (currentTime - previousFrame >= 65) {
+      previousFrame = currentTime;
+      Camera.readFrame(frameBuffer.data());
+      mqtt.publish("/smartcar/camera", frameBuffer.data(), frameBuffer.size(),
+                   false, 0);
+    }
+#endif
+    static auto previousTransmission = 0UL;
+    if (currentTime - previousTransmission >= oneSecond) {
+      previousTransmission = currentTime;
+      const auto distance = String(front.getDistance());
+      mqtt.publish("/smartcar/ultrasound/front", distance);
+    }
+  }
+#ifdef __SMCE__
+  // Avoid over-using the CPU if we are running in the emulator
   delay(1);
 #endif
 }
