179af20bcf62eb397a0141dac0daafb0b3499d21
annatorngren <annato@student.chalmers.se>
2022-05-05 02:06:18 +0200

    adds screens in Drawer Menu

--- GreenGarbageApp/app/src/main/java/com/example/greengarbageapp/activities/ContactFragment.kt
+++ GreenGarbageApp/app/src/main/java/com/example/greengarbageapp/activities/ContactFragment.kt
@@ -5,16 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.greengarbageapp.R
 import com.example.greengarbageapp.databinding.FragmentAboutBinding
-import com.example.greengarbageapp.databinding.FragmentEndBinding
+import com.example.greengarbageapp.databinding.FragmentContactBinding
 
-
-class AboutFragment : Fragment() {
-
-
+class ContactFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentAboutBinding.inflate(inflater, container, false)
+        val binding = FragmentContactBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
--- GreenGarbageApp/app/src/main/java/com/example/greengarbageapp/activities/UNinfoFragment.kt
+++ GreenGarbageApp/app/src/main/java/com/example/greengarbageapp/activities/UNinfoFragment.kt
@@ -5,16 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.greengarbageapp.R
 import com.example.greengarbageapp.databinding.FragmentAboutBinding
-import com.example.greengarbageapp.databinding.FragmentEndBinding
+import com.example.greengarbageapp.databinding.FragmentUNinfoBinding
 
-
-class AboutFragment : Fragment() {
-
+class UNinfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentAboutBinding.inflate(inflater, container, false)
+        val binding = FragmentUNinfoBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
