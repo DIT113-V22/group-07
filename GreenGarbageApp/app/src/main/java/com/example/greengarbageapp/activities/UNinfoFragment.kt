64c1fb085ce322c9bcb046737ed6de8555d67bc9
Anna <gusolsang@student.gu.se>
2022-05-10 10:02:31 +0200

    update LinearLayout on all layouts

--- GreenGarbageApp/app/src/main/java/com/example/greengarbageapp/activities/UNinfoFragment.kt
+++ GreenGarbageApp/app/src/main/java/com/example/greengarbageapp/activities/UNinfoFragment.kt
@@ -1,5 +1,7 @@
 package com.example.greengarbageapp.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +14,11 @@ class UNinfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentUNinfoBinding.inflate(inflater, container, false)
+        binding.UNLinkTv.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://sdgs.un.org/goals/goal12")
+            startActivity(openURL)
+        }
         return binding.root
     }
 }
