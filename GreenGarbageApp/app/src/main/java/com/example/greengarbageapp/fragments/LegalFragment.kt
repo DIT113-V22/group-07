887dbfc3c7ef69758c16076fa33513c5d496524f
A.Törngren <91566171+Toerngren@users.noreply.github.com>
2022-05-25 14:22:06 +0200

    Revert "Clean up"

---
+++ GreenGarbageApp/app/src/main/java/com/example/greengarbageapp/fragments/LegalFragment.kt
@@ -0,0 +1,17 @@
+package com.example.greengarbageapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.greengarbageapp.databinding.FragmentLegalBinding
+
+class LegalFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentLegalBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
